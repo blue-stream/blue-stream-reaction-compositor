1177369c16da3244200d064305f1934fb7bf58e8
Almog Vagman Ciprut <almogvc@gmail.com>
2019-03-17 17:11:31 +0200

    fix: check req.user existence

--- src/utils/errors/errorHandler.ts
+++ src/utils/errors/errorHandler.ts
@@ -5,7 +5,7 @@ import { AxiosError } from 'axios';
 export function errorHandler(error: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
     if ((error as AxiosError).response) {
         const axiosError = error as AxiosError;
-        log('warn' , 'AxiosError', `${error.name} was thrown with status ${axiosError.response!.status} and message ${axiosError.response!.statusText}`, '', req.user.id);
+        log('warn' , 'AxiosError', `${error.name} was thrown with status ${axiosError.response!.status} and message ${axiosError.response!.statusText}`, '', req.user ? req.user.id : 'unknown');
 
         res.status(axiosError.response!.status).send();
     } else {
@@ -14,7 +14,7 @@ export function errorHandler(error: Error, req: express.Request, res: express.Re
 }
 
 export function unknownErrorHandler(error: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
-    log('error' , 'Unknown Error', `${error.name} was thrown with status 500 and message ${error.message}`, '', req.user.id);
+    log('error' , 'Unknown Error', `${error.name} was thrown with status 500 and message ${error.message}`, '', req.user ? req.user.id : 'unknown');
 
     res.status(500).send({
         type: error.name,
