f728ec39acd38fcf65561506ed6f3a51ecb5ed83
Almog Vagman Ciprut <almogvc@gmail.com>
2019-06-08 19:27:51 +0300

    refactor: add getUserLikedVideos and added populate videos function

--- src/reactions/reactions.controller.ts
+++ src/reactions/reactions.controller.ts
@@ -17,38 +17,50 @@ export class ReactionsController {
         res.json(await ReactionsService.create(req.body, req.headers.authorization!));
     }
 
-    static async canGetReactionAmountByTypeAndResourceType(req: Request, res: Response) {
+    static async getReactionAmountByTypeAndResourceType(req: Request, res: Response) {
         const resourceReactions: any[] = await ReactionsService.getReactionAmountByTypeAndResourceType(req.query, req.headers.authorization!);
-        const videosIds = resourceReactions.map((resourceReaction: any) => resourceReaction.resource);
 
-        const videosMap = await VideosRpc.getVideos(videosIds, req.user.id).catch((error) => {
-            log('warn', 'Video Rpc request failed - getVideos', error.message, '', req.user ? req.user.id : 'unknown', { error });
+        res.json(await ReactionsController.popolateVideos(resourceReactions, req.user));
+    }
+
+    static async getUserLikedVideos(req: Request, res: Response) {
+        const reactions: any[] = await ReactionsService.getMany(req.query, req.headers.authorization!);
+
+        res.json(await ReactionsController.popolateVideos(reactions, req.user));
+    }
+
+    private static async popolateVideos(reactions: any[], user: any) {
+        const videosIds = reactions.map((reaction: any) => reaction.resource);
+        const videosMap = await VideosRpc.getVideos(videosIds, user.id).catch((error) => {
+            log('warn', 'Video Rpc request failed - getVideos', error.message, '', user ? user.id : 'unknown', { error });
             return [];
         });
 
         const channelsIds = Object.keys(videosMap).map((video: any) => video.channel);
         const channelsMap = await ChannelsRpc.getChannelsByIds(channelsIds).catch((error) => {
-            log('warn', 'Channels Rpc request failed - getChannelsByIds', error.message, '', req.user ? req.user.id : 'unknown', { error });
+            log('warn', 'Channels Rpc request failed - getChannelsByIds', error.message, '', user ? user.id : 'unknown', { error });
             return [];
         });
 
-        const reactionWithResource = resourceReactions.map(
-            (resourceReaction) => {
+        const reactionWithResource = reactions
+            .map((reaction) => {
                 return {
-                    ...resourceReaction,
-                    resource: videosMap[resourceReaction.resource] || resourceReaction.resource,
+                    ...reaction,
+                    resource: videosMap[reaction.resource] || reaction.resource,
                 };
             });
 
-        return res.json(reactionWithResource.map(
-            (resourceReaction) => {
+            return reactionWithResource
+            .map((reaction) => {
                 return {
-                    ...resourceReaction,
+                    ...reaction,
                     resource: {
-                        ...resourceReaction.resource,
-                        channel: channelsMap[resourceReaction.resource.channel] || resourceReaction.resource.channel,
+                        ...reaction.resource,
+                        channel: reaction.resource.channel ?
+                            channelsMap[reaction.resource.channel] || reaction.resource.channel :
+                            undefined,
                     },
                 }
-            }));
+            });
     }
 }
