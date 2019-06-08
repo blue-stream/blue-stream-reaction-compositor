ce21342a292316de5d46be42909edcc0decad36b
Almog Vagman Ciprut <almogvc@gmail.com>
2019-06-08 12:01:48 +0300

    refactor: add channels and videos rpc requests

--- src/config.ts
+++ src/config.ts
@@ -11,9 +11,24 @@ export const config = {
             api: process.env.USERS_API || '/api/reaction',
         },
         videos: {
+            rpc: {
+                port: +(process.env.VIDEOS_RPC_PORT || 6001),
+                methods: {
+                    GET_VIDEOS: 'getVideos',
+                },
+            },
             port: +(process.env.VIDEOS_PORT || 5001),
             hostname: process.env.VIDEOS_HOST || 'http://localhost',
             api: process.env.USERS_API || '/api/video',
+        },
+        channels: {
+            rpc: {
+                port: +(process.env.CHANNELS_RPC_PORT || 6006),
+                methods: {
+                    GET_CHANNELS_BY_IDS: 'getChannelsByIds',
+                },
+            },
+            hostname: process.env.CHANNELS_HOST || 'http://localhost',
         },
     },
     server: {
